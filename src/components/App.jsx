33b9015b6f53f0feb073ec41357d0f5e909df00f
millenialia <lenalebo@gmail.com>
2023-08-13 13:43:43 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,9 @@
-import {  useEffect } from 'react'
-
 import { ContactForm } from 'components/ContactForm/ContactForm'
 import { ContactList } from "components/ContactList/ContactList";
 import { Filter } from "components/Filter/Filter";
 
 
 export const App = () => {
-
-  // useEffect(() => {
-  //   setContacts(JSON.parse(localStorage.getItem('contacts')) || [])
-  // }, []);
-
-  // useEffect(() => {
-  //   localStorage.setItem('contacts', JSON.stringify(contacts));
-  // }, [contacts])
-
-
-
     return (
       <div className='phonebookBlock'>
         <h1>Phonebook</h1>
